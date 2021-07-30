5c1af209177c27eda5dfce382e1cfff4b7e6e146
Rola-H <rolahararah@gmail.com>
2021-07-30 15:49:50 +0300

    modify js file >Relates #21

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-fetch('https://corona.lmao.ninja/v2/countries/usa')
-.then(function(response)  {
-    return response.json();
-  })
-  .then(function(data){
-    console.log(data);
-    // document.getElementById("country").innerHTML = data.country
-    document.getElementById("#flag ").src = data.countryInfo.flag;
-    document.getElementById("cases").innerHTML = data.cases;
-    document.getElementById("recovered").innerHTML = data.recovered;
-    document.getElementById("death").innerHTML = data.death;
-  });
+// let countryName=document.querySelector("input.val");
+// window.onload = function(){  
+//   countryName="Palestine"
+// };
+
+
+fetch('https://corona.lmao.ninja/v2/countries/Palestine')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+  document.getElementById("flag").src = data.countryInfo.flag;
+//   document.getElementById("country").innerHTML = data.country;
+  document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+  document.getElementById("death").innerHTML = data.deaths.toLocaleString();
+  document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+});
